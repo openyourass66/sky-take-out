afc99ed3e2ec1eac822e3d7b6201869c8b080b21
张曦 <2628506582@qq.com>
2026-01-27 16:07:59 +0800

    销量排名统计业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -61,5 +62,15 @@ public class ReportController {
         log.info("用户统计：{}到{}", begin, end);
         OrderReportVO ordersReportVO = reportService.getOrdersStatistics(begin, end);
         return Result.success(ordersReportVO);
+    }
+    @GetMapping("/top10")
+    @ApiOperation("销量排名")
+    public Result<SalesTop10ReportVO> top10 (
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate end)
+    {
+        log.info("销量排名：{}到{}", begin, end);
+        SalesTop10ReportVO salesTop10ReportVO = reportService.getSalesTop10(begin,end);
+        return Result.success(salesTop10ReportVO);
     }
 }
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
@@ -29,4 +30,12 @@ public interface ReportService {
      * @return
      */
     OrderReportVO getOrdersStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 销量
+     * @param begin
+     * @param end
+     * @return
+     */
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 }
